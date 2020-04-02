721af2811e9cf6665241e80ef52eb0846bdc77ee
BrunoSilva98 <brunops98@hotmail.com>
2020-04-02 17:04:14 -0300

    Adição de comentarios explicando funcionamento

--- Perceptron.py
+++ Perceptron.py
@@ -1,4 +1,27 @@
-# Inputs devem ser no formato de lista aninhada, ex [[1, 2], [3,4]]
+# Existem duas formas de utilizar
+# 1 - Somente um Neuronio
+# 2 - Mais de um Neuronio
+# Em cada um dos modos, o shape do input e output para TREINAMENTO devem ser diferentes
+# Shape do input com somente um neuronio: Entrada = [[1, 0, 1, 0, 1], [0, 1, 0, 1, 0]], ou seja, sublistas
+# Deve ser utilizado uma sublista mesmo que haja somente uma entrada, EX -> [[1, 0, 1, 0]]
+# O Output deve ser fornecido da seguinte maneira quando há somente um neuronio:
+# Output = [0, 1], ou seja, um array simples.
+# A Proporção de Input/Output deve ser de 1/1, ou seja, um valor no array de saída para cada subArray no input
+
+# A Segunda Forma para treinamento são com multiplos neuronios
+# Nesse caso, o input permanece da mesma maneira, sublistas dentro de listas, mesmo que com somente um elemento
+# O Output deve vir dentro de uma sublista, ou seja, se o input for [[1,2,3]] e houverem 3 neurônios
+# O output deve vir da seguinte maneira: [[0, 1, 2]]
+# Cada um dos valores da sublista de output indica a saida de um neuronio
+# Caso existam mais neurônios que saídas, os neurônios excedentes serão ignorados
+# Caso existam mais saídas que neurônios, ocorrerá erro.
+# É possível realizar treinamento de multiplas classes ao mesmo tempo, ou seja:
+# Entrada = [[0, 1, 0, 1], [1, 0, 1, 0]], duas classes de entrada
+# Saida = [[0,1], [1, 0]]
+
+# Finalizado o treinamento seguindo o padrão acima, para realizar alguma previsão basta chamar a função calcula_saida
+# Recebe como parâmetro somente o input de um array simples, ou seja, [0, 1, 0, 1]
+# Retornará o output no mesmo formato de array
 
 from Perceptron_Neuron import Neuron
 
